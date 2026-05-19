37357eeabcc43d398ad8a69a6835be7f45fde8a9
Adam Khairi <57996941+adamkhairi@users.noreply.github.com>
2026-05-20 00:01:08 +0100

    chore: add generated Tailwind CSS file for production build

---
+++ public/build/assets/check-dSxBx-J8.js
@@ -0,0 +1,1 @@
+import{k as e}from"./app-D_G1SEzN.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
---
+++ public/build/assets/loader-circle-CRjRyfNa.js
@@ -0,0 +1,1 @@
+import{k as e}from"./app-D_G1SEzN.js";var t=e(`LoaderCircle`,[[`path`,{d:`M21 12a9 9 0 1 1-6.219-8.56`,key:`13zald`}]]);export{t};
